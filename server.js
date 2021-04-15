01824faa2327fa2422bde89eeb2b3351346faf1f
miguebeto <mindmusic7@gmail.com>
2021-04-15 18:16:23 -0500

    agregado postman, mongodb y configurado mongoclient

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const MongoClient = require('mongodb').MongoClient;
 
 app.listen(3000,()=>{console.log(`listening on  ${port}`)});
-
 app.get('/', (req, res)=>{res.send('Hello World')});
+MongoClient.connect('mongodb://localhost/crud-express', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.error(err);
+    console.log('Connected to Database');
+});
